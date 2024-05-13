6ddd704131b342f8a98da0e022ea398cea5437da
deniss673 <denis.denis67889@gmail.com>
2024-05-14 01:49:46 +0300

    added duration bar

--- Assets/UiConsumables.cs
+++ Assets/UiConsumables.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,21 +11,37 @@ public class UiConsumables : MonoBehaviour
 
     public Sprite[] ConsumablesItems;
 
+    public GameObject[] buffs;
+
+    public GameObject slider;
+
     public void AddItemUi(int index)
     {
         
 
-        GameObject newImage = new GameObject("My Image");
+        GameObject newImage = new GameObject("buff " + buffs.Length);
         newImage.AddComponent<Image>();
 
         newImage.GetComponent<Image>().sprite = ConsumablesItems[index];
-
-
-        //newImage.GetComponent<Image>().sprite = UIimage;
-
-        
-       
+               
         newImage.transform.SetParent(GameObject.FindWithTag("scrollView").transform, false);
-       
+
+        RectTransform pos = newImage.GetComponent<RectTransform>();
+
+        pos.anchoredPosition = new Vector2(-80, 30 - 100 * buffs.Length);
+
+        pos.sizeDelta = new Vector2(80, 80);
+
+        buffs.Append(newImage);
+
+        GameObject test = Instantiate(slider, new Vector3(105,-0,0), Quaternion.identity);
+
+        test.transform.SetParent(newImage.transform, false);
+
+        test.GetComponent<DurationBarScript>().TimeLeft();
+
+
+
+        //newImage.
     }
 }

9a49570b97f9e6dc75380f6929a5b1712da93965
deniss673 <denis.denis67889@gmail.com>
2024-05-14 13:43:43 +0300

    ui for item duration almost done

--- Assets/UiConsumables.cs
+++ Assets/UiConsumables.cs
@@ -11,15 +11,49 @@ public class UiConsumables : MonoBehaviour
 
     public Sprite[] ConsumablesItems;
 
-    public GameObject[] buffs;
+    public List<GameObject> buffs = new List<GameObject>();
 
     public GameObject slider;
 
+    void Update()
+    {
+        for (int i = buffs.Count - 1; i >= 0; i--)
+        {
+            if (buffs[i] == null)
+            {
+                buffs.RemoveAt(i);
+                backInPlace(i);
+                i++;
+            }
+        }
+    }
+
+    public void backInPlace(int index)
+    {
+        for(int i = index; i < buffs.Count(); i++)
+        {
+            GameObject buff=buffs[i];
+
+            buff.GetComponent<RectTransform>().anchoredPosition = new Vector2(-80, 30 - 100 * i);
+
+            Transform sliderCloneTransform = buff.transform.GetChild(0);
+
+            GameObject sliderClone = sliderCloneTransform.gameObject;
+
+            
+
+
+
+        }
+    }
+
     public void AddItemUi(int index)
     {
-        
 
-        GameObject newImage = new GameObject("buff " + buffs.Length);
+        Debug.Log(buffs);
+
+
+        GameObject newImage = new GameObject("buff " + buffs.Count());
         newImage.AddComponent<Image>();
 
         newImage.GetComponent<Image>().sprite = ConsumablesItems[index];
@@ -28,20 +62,24 @@ public class UiConsumables : MonoBehaviour
 
         RectTransform pos = newImage.GetComponent<RectTransform>();
 
-        pos.anchoredPosition = new Vector2(-80, 30 - 100 * buffs.Length);
+        pos.anchoredPosition = new Vector2(-80, 30 - 100 * buffs.Count());
 
         pos.sizeDelta = new Vector2(80, 80);
 
-        buffs.Append(newImage);
+        buffs.Add(newImage);
+
+        Debug.Log(buffs.Count());
 
         GameObject test = Instantiate(slider, new Vector3(105,-0,0), Quaternion.identity);
 
         test.transform.SetParent(newImage.transform, false);
 
-        test.GetComponent<DurationBarScript>().TimeLeft();
+       
+        
+        test.GetComponent<DurationBarScript>().TimeLeft(newImage);
 
 
 
-        //newImage.
+        
     }
 }
